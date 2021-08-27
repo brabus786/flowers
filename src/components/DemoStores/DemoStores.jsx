dd2bfde128bc53b5541deb037c4ca34445c9633c
Nick Pilipchatin <50636011+brabus786@users.noreply.github.com>
2021-08-27 09:19:52 +0300

    fix

--- src/components/DemoStores/DemoStores.jsx
+++ src/components/DemoStores/DemoStores.jsx
@@ -5,6 +5,7 @@ import { Container, Row, Col } from 'reactstrap';
 import Logo from '../Logo/Logo';
 import mokap1 from '../../img/mockuper7.png';
 import mokap2 from '../../img/mockuper8.png';
+import mokap3 from '../../img/mockuper9.png';
 
 
 const DemoStores = ({ setIsPopup }) => {
@@ -27,6 +28,7 @@ const DemoStores = ({ setIsPopup }) => {
                             </a>
                         </div>
                     </Col>
+                  
                     <Col className='DemoStores__col buttonFlowers' xl='4'>
                         <p className='DemoStores__title'>Магазин саженцев</p>
                         <a target="_blank" href="https://app.florinka.pp.ua/public/point?category=6113f0c25c3756003b283ce7">
@@ -38,9 +40,19 @@ const DemoStores = ({ setIsPopup }) => {
                             </a>
                         </div>
                     </Col>
-                    <Col xl='4'>
 
+                    <Col className='DemoStores__col buttonFlowers' xl='4'>
+                        <p className='DemoStores__title'>Магазин декора</p>
+                        <a target="_blank" href="https://app.florinka.pp.ua/public/decor?category=61287ea4856aaa003c9de641">
+                            <img src={mokap3} alt="mokap1" />
+                        </a>
+                        <div className='DemoStores__button'>
+                            <a className='lickStore' target="_blank" href="https://app.florinka.pp.ua/public/decor?category=61287ea4856aaa003c9de641">
+                                <span className='button'>Смотреть</span>
+                            </a>
+                        </div>
                     </Col>
+                  
                 </Row>
             </Container>
         </div>
